8420a3837cdc0cf2ce78e467dec07e17c25fa5b6
Roman <astek14@mail.ru>
2025-08-16 12:08:50 +0300

    Create tables

---
+++ src/com/dmdev/script/lesson5.sql
@@ -0,0 +1,9 @@
+CREATE TABLE company_storage.company (
+                                         id INT,
+                                         name VARCHAR(128),
+                                         date DATE
+);
+
+-- DROP TABLE public.company;
+
+-- https://postgrespro.ru/docs/postgresql/current/datatype
