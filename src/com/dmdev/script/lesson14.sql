dd096bc5bdce286bfa5c99802b70511a6bf617d2
Roman <astek14@mail.ru>
2025-08-16 14:44:43 +0300

    DELETE

---
+++ src/com/dmdev/script/lesson14.sql
@@ -0,0 +1,38 @@
+DELETE
+FROM employee
+WHERE salary IS NULL;
+
+DELETE
+FROM employee
+WHERE salary = (SELECT max(salary) FROM employee);
+
+DELETE
+FROM company
+WHERE id = 2;
+
+DELETE
+FROM employee
+WHERE company_id = 1;
+
+SELECT *
+FROM employee;
+
+CREATE TABLE employee
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(128) NOT NULL,
+    last_name  VARCHAR(128) NOT NULL,
+    company_id INT REFERENCES company (id) ON DELETE CASCADE,
+    salary     INT,
+    UNIQUE (first_name, last_name)
+--     FOREIGN KEY (company_id) REFERENCES company (id)
+);
+
+DROP TABLE employee;
+
+INSERT INTO employee(first_name, last_name, salary, company_id)
+VALUES ('Ivan', 'Sidorov', 500, 1),
+       ('Ivan', 'Ivanov', 1000, 2),
+       ('Arni', 'Paramonov', NULL, 2),
+       ('Petr', 'Petrov', 2000, 3),
+       ('Sveta', 'Svetikova', 1500, NULL);
